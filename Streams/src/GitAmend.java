a857df69a863ce7db6224c14d58da63637d4de62
hasannaqvi1 <naqvih3@gmail.com>
2024-10-21 16:29:28 +0530

    Added the comments initially, added new comments using amend

--- Streams/src/GitAmend.java
+++ Streams/src/GitAmend.java
@@ -1,2 +1,11 @@
 public class GitAmend {
+    /*Git amend is used to modify the most recent commit
+    * It is recommended to amend only those files that
+    * are not pushed else you will need git push --force
+    * Supose you missed to commit a file and then  you
+    * can fist add then use git amend*/
+
+    /*Added new comment to test amend command*/
+
+
 }
